5cbfb1a5eb4adacb03b85c13c27eb8ca65c60d94
tudor127 <48529719+tudor127@users.noreply.github.com>
2020-01-11 14:02:18 +0200

    Add files via upload

---
+++ DJANGO_PRROJECT/IA_APP/static/js/javascript.js
@@ -0,0 +1,28 @@
+var messagesCount=0;
+$(function() {
+    $("#messageInput").keypress(function (e) {
+        if(e.which == 13) {
+            //submit form via ajax, this is not JS but server side scripting so not showing here
+            messagesCount++;
+            messageId="message_"+messagesCount;
+            $("#messagesBox").append("<div class='userMessage' id='"+messageId+"'>"+$(this).val() + "</div>");
+            $("#messagesBox").append("<div class='botMessage'>Hello John</div>");
+    		$('#messagesBox ').animate({
+        	scrollTop: $('#'+messageId).position().top
+    	}, 'slow');
+    		  $(this).val("");
+            e.preventDefault();
+        }
+    });
+});
+
+function getBotReply(str) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("demo").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("POST", "bot_response/?text="+, true);
+  xhttp.send();
+}
