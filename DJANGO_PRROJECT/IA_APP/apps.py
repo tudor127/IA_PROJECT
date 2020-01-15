b75aad5da475ca968788f4db6515cb71baa807a7
tudor127 <48529719+tudor127@users.noreply.github.com>
2020-01-15 16:49:56 +0200

    revert push

--- DJANGO_PRROJECT/IA_APP/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class IaAppConfig(AppConfig):
-    name = 'IA_APP'
--- DJANGO_PRROJECT/IA_APP/test.py
+++
@@ -1,9 +0,0 @@
-import aiml
-
-# Create the kernel and learn AIML files
-kernel = aiml.Kernel()
-kernel.learn("aiml/test.xml")
-
-# Press CTRL-C to break this loop
-while True:
-    print(kernel.respond(input("Enter your message >> ")))
